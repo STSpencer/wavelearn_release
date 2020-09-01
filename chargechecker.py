4ff960031867edff24af83bcd9754600d69a6d32
STSpencer <sojourner92@gmail.com>
2020-09-01 14:10:31 +0100

    Updated figures

--- chargechecker.py
+++ chargechecker.py
@@ -11,15 +11,7 @@ chargesums2=[]
 cutsums2=[]
 count=0
 count2=0
-
-import matplotlib.ticker as mtick
-def div_5(x, *args):
-    """
-    The function that will you be applied to your y-axis ticks.
-    """
-    x = float(x)/5.0
-    return "{:.1f}".format(x)
-   
+bins=np.linspace(10**5.5,10**8)
 for file in onlyfiles:
     count=count+1
     print(count,'/',len(onlyfiles))
@@ -35,9 +27,9 @@ for file in onlyfiles:
         plt.hlines(40,8,40,colors='r')
         plt.vlines(8,8,40,colors='r')
         plt.vlines(40,8,40,colors='r')
-        plt.xlabel('x Angular Size on Sky ($^\circ$)',size='large')
-        plt.ylabel('y Angular Size on Sky ($^\circ$)',size='large')
-        
+        plt.xlabel('x (Pixels)',size='large')
+        plt.ylabel('y (Pixels)',size='large')'''
+        '''
         plt.hlines(8,0,8,colors='b')
         plt.hlines(8,40,47,colors='b')
         plt.hlines(40,0,8,colors='b')
@@ -45,15 +37,8 @@ for file in onlyfiles:
         plt.vlines(8,0,8,colors='b')
         plt.vlines(8,40,47,colors='b')
         plt.vlines(40,0,8,colors='b')
-        plt.vlines(40,40,47,colors='b')
-        
-        ax = plt.gca()       
-        ax.yaxis.set_major_formatter(mtick.FuncFormatter(div_5))
-        ax.xaxis.set_major_formatter(mtick.FuncFormatter(div_5))
-        cb=plt.colorbar()
-        cb.set_label('Charge (Relative Units)')
+        plt.vlines(40,40,47,colors='b')'''
         plt.show()
-        '''
         chargesums.append(np.sum(chargearr[i,:,:,:]))
         cutsums.append(np.sum(chargearr[i,:,8:40,8:40]))
     inputdata.close()
@@ -61,13 +46,14 @@ for file in onlyfiles:
 fig, (ax1, ax2) = plt.subplots(1, 2)
 chargesums=np.asarray(chargesums)
 cutsums=np.asarray(cutsums)
-ax1.hist(chargesums,bins=100,label='48x48',alpha=0.2)
-ax1.hist(cutsums,bins=100,label='32x32',alpha=0.2)
+ax1.hist(chargesums,bins=bins,label='48x48',alpha=0.2)
+ax1.hist(cutsums,bins=bins,label='32x32',alpha=0.2)
 ax1.set_xlabel('Total Charge (Relative Units)',size='large')
-ax1.set_ylabel('Frequency (Counts)',size='large')
+ax1.set_ylabel('Number of Events (Counts)',size='large')
 ax1.set_title('Point Source Run',size='large')
-ax1.semilogy()
+ax1.loglog()
 ax1.legend(fontsize='large')
+ax1.set_xlim(630957,1e8)
 onlyfiles = sorted(glob.glob('/store/spencers/Data/diffuserun1/*.hdf5'))
 
 for file in onlyfiles:
@@ -82,14 +68,15 @@ for file in onlyfiles:
 
 chargesums2=np.asarray(chargesums2)
 cutsums2=np.asarray(cutsums2)
-ax2.hist(chargesums2,bins=100,label='48x48',alpha=0.2)
-ax2.hist(cutsums2,bins=100,label='32x32',alpha=0.2)
-ax2.set_xlabel('Total Charge (p.e.)',size='large')
-ax2.set_ylabel('Frequency (Counts)',size='large')
+ax2.hist(chargesums2,bins=bins,label='48x48',alpha=0.2)
+ax2.hist(cutsums2,bins=bins,label='32x32',alpha=0.2)
+ax2.set_xlabel('Total Charge (Relative Units)',size='large')
+ax2.set_ylabel('Number of Events (Counts)',size='large')
 ax2.set_title('Diffuse Run',size='large')
-ax2.semilogy()
+ax2.loglog()
+ax2.set_xlim(630957,1e8)
 ax2.legend(fontsize='large')
 plt.tight_layout()
-plt.savefig('/home/spencers/Figures/chargehist.png',figsize=(11.69, 8.27), dpi=300)
+plt.savefig('/home/spencers/Figures/chargehistlog10.png',figsize=(8.27, 14), dpi=300)
 plt.show()
 
